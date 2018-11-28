6a80edd919aef5913d34ba4977dd9fdac23cc2d5
Markus Brachner <Markus.Brachner@sintef.no>
2018-11-28 23:14:39 +0100

    Add documentation to XPDrawingPhase

--- XPNet.CLR/Display/XPlaneDisplay.cs
+++ XPNet.CLR/Display/XPlaneDisplay.cs
@@ -84,20 +84,73 @@ namespace XPNet
 	{
 		// MAINT: This needs to be kept in sync with the
 		// XPLMDrawingPhase enumerable from the X-Plane API
+		/// <summary>
+		/// This is the earliest point at which you can draw in 3-d.
+		/// </summary>
 		FirstScene = 0,
+
+		/// <summary>
+		/// Drawing of land and water.
+		/// </summary>
 		Terrain = 5,
+
+		/// <summary>
+		/// Drawing runways and other airport detail.
+		/// </summary>
 		Airports = 10,
+
+		/// <summary>
+		/// Drawing roads, trails, trains, etc.
+		/// </summary>
 		Vectors = 15,
+
+		/// <summary>
+		/// 3-d objects (houses, smokestacks, etc.
+		/// </summary>
 		Objects = 20,
+
+		/// <summary>
+		/// External views of airplanes, both yours and the AI aircraft.
+		/// </summary>
 		Airplanes = 25,
+
+		/// <summary>
+		/// This is the last point at which you can draw in 3-d.
+		/// </summary>
 		LastScene = 30,
+
+		/// <summary>
+		/// This is the first phase where you can draw in 2-d.
+		/// </summary>
 		FirstCockpit = 35,
+
+		/// <summary>
+		/// The non-moving parts of the aircraft panel.
+		/// </summary>
 		Panel = 40,
+
+		/// <summary>
+		/// The moving parts of the aircraft panel.
+		/// </summary>
 		Gauges = 45,
+
+		/// <summary>
+		/// Floating windows from plugins.
+		/// </summary>
 		Window = 50,
+
+		/// <summary>
+		/// The last change to draw in 2d.
+		/// </summary>
 		LastCockpit = 55,
+
+		[Obsolete("Removed as of XPLM300; Use the full-blown XPLMMap API instead.")]
 		LocalMap3D = 100,
+
+		[Obsolete("Removed as of XPLM300; Use the full-blown XPLMMap API instead.")]
 		LocalMap2D = 101,
+
+		[Obsolete("Removed as of XPLM300; Use the full-blown XPLMMap API instead.")]
 		LocalMapProfile = 102
 	}
 }
